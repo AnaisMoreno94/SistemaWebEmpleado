776b750047346199653e88f13a6ee3f33e73ec00
Anais Moreno <anabet.94@gmail.com>
2023-01-31 17:01:35 -0300

    Agregar archivos de proyecto.

---
+++ PracticaIntegradoraMVC/Models/Empleado.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
+using SistemaWebEmpleado.Validators;
+
+namespace SistemaWebEmpleado.Models
+{
+    public class Empleado
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        [Column(TypeName = "varchar(50)")]
+        [StringLength(50)]
+        public string Nombre { get; set; }
+
+
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        [Column(TypeName = "varchar(50)")]
+        [StringLength(50)]
+        public string Apellido { get; set; }
+
+        public int DNI { get; set; }
+
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        [RegularExpression("[A-Z]{1}[0-9]{5}")]
+        public string Legajo { get; set; }
+        
+        [CheckValidYearAtributte]
+        public DateTime FechaNacimiento { get; set; }
+
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        [Column(TypeName = "varchar(50)")]
+        [StringLength(50)]
+        public int Titulo { get; set; }
+
+    }
+}
---
+++ PracticaIntegradoraMVC/Validators/CheckValidYearAtributte.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http.Features;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SistemaWebEmpleado.Validators
+{
+    public class CheckValidYearAtributte : ValidationAttribute
+    {
+        public CheckValidYearAtributte() 
+        {
+            ErrorMessage = "El año de nacimiento debe ser mayor a 2000";
+        }
+        public override bool IsValid(object value)
+        {
+            DateTime fechaNacimiento = (DateTime)value;
+            int year = fechaNacimiento.Year;
+
+            if (year < 2000) return false; else return true;
+
+        }
+    }
+}
